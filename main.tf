442e62faebd98ddafaa8490c6b800fb4be267f18
adenicole <dedoyinnicole@gmail.com>
2021-09-20 12:41:21 +0100

    Created manifest to deploy 20 instances

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "aws" {
   profile = "default"
 }
 
+
 resource "aws_instance" "terra" {
+  count         = "20"
   ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
+  key_name   = "terraform"
 }
